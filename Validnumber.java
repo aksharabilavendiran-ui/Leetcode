4f303881d5f80fb4fca6cd1f3f27f186e195ebdb
AKSHARA B <aksharabilavendiran@gmail.com>
2026-07-23 14:09:20 +0530

    Implement isNumber method to validate numeric strings

---
+++ Validnumber.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isNumber(String s) {
+        s = s.trim(); 
+        boolean seenDigit = false, seenDot = false, seenExp = false;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (Character.isDigit(c)) {
+                seenDigit = true;
+            } else if (c == '.') {
+                if (seenDot || seenExp) return false;
+                seenDot = true;
+            } else if (c == 'e' || c == 'E') {
+                if (seenExp || !seenDigit) return false;
+                seenExp = true;
+                seenDigit = false; 
+            } else if (c == '+' || c == '-') {
+                if (i > 0 && s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E') return false;
+            } else {
+                return false;
+            }
+        }
+        return seenDigit;
+    }
+}
